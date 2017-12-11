a11a19bb9391a26f62f3477bebb05ff4a22439dd
zhoukaiyun <2275116092@qq.com>
2017-12-11 20:14:48 +0800

    swiper

--- swiper/js/script.js
+++ swiper/js/script.js
@@ -7,5 +7,16 @@ var swiper = new Swiper('.swiper-container', {
          // 如果需要前进后退按钮
     navigation: {
       nextEl: '.swiper-button-next'
+    },
+//Swiper4.x
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
     }
-    });
+
+});
