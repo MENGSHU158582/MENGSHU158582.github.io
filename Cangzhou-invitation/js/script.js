5346a406b6ecb02fe2a35e3cbf7164b4df6f1157
MENGSHU158582 <2275116092@qq.com>
2017-12-15 17:30:22 +0800

    1

--- Cangzhou-invitation/js/script.js
+++ Cangzhou-invitation/js/script.js
@@ -1,33 +1,43 @@
 var swiper = new Swiper('.swiper-container', {
-      direction: 'vertical',
-      pagination: {
+    direction: 'vertical',
+    pagination: {
         el: '.swiper-pagination',
         clickable: true,
-      },
-         // 如果需要前进后退按钮
+    },
+    // 如果需要前进后退按钮
     navigation: {
-      nextEl: '.swiper-button-next'
+        nextEl: '.swiper-button-next'
     },
 //Swiper4.x
-    on:{
-        init: function(){
+    on: {
+        init: function () {
             swiperAnimateCache(this); //隐藏动画元素
             swiperAnimate(this); //初始化完成开始动画
         },
-        slideChangeTransitionEnd: function(){
+        slideChangeTransitionEnd: function () {
             swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
         }
     }
 });
 // 音乐
-var mp3=document.querySelector("#mp3");
-var mp3btn=document.querySelector("#mp3btn");
-mp3btn.addEventListener('click',function () {
-    if(mp3.paused){
+var mp3 = document.querySelector("#mp3");
+var mp3btn = document.querySelector("#mp3btn");
+mp3btn.addEventListener('click', function () {
+    if (mp3.paused) {
         mp3.play();
-        mp3btn.style.animationPlayState='running';
-    }else {
+        mp3btn.style.animationPlayState = 'running';
+    } else {
         mp3.pause();
-        mp3btn.style.animationPlayState='paused';
+        mp3btn.style.animationPlayState = 'paused';
     }
 });
+
+
+setTimeout(function () {
+    document.querySelector(".chuan").style.animationName = 'ship';
+    document.querySelector(".chuan2").style.animationName = 'ship';
+    //var a = document.querySelectorAll(".chuan");
+    //for (i = 0; i < a.length; i++) {
+    //    a.style.animationName = 'ship';
+    //}
+}, 3500);
